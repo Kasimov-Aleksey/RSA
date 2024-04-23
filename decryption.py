1a75f0ea382b3ad5ac2e21e3eae1f498dac1f870
Kasimov-Aleksey <leha.kasimov@mail.ru>
2024-04-23 13:15:54 +0300

    decrypte_demo

--- decryption.py
+++ decryption.py
@@ -7,12 +7,12 @@ def calling_privat_key_generation():
         public_key = public_key.readlines()
         for key in public_key:
             data_keys.append(int(key[:-1]))
-    print({
-        'private_key': data_keys[0],
-        'modulus': data_keys[1],
-        'private_key_length': len(bin(data_keys[0])[2:]),
-        'modulus_length': len(bin(data_keys[1])[2:])
-    })
+    # print({
+    #     'private_key': data_keys[0],
+    #     'modulus': data_keys[1],
+    #     'private_key_length': len(bin(data_keys[0])[2:]),
+    #     'modulus_length': len(bin(data_keys[1])[2:])
+    # })
     return data_keys
 
 def get_len_block(data_keys):
@@ -36,21 +36,37 @@ def decryptor(data_keys, data_blocks_text):
     data_num = []
     for num in data_blocks_text:
         data_num.append(pow(num, data_keys[0], data_keys[1]))
-    print(data_num)
+    # print(data_num)
     return data_num
+
+
+def checker_bits(data_num):
+    sum_bits = 0
+    for bits in data_num:
+        bit = bin(bits)[2:].zfill(len_block)
+        sum_bits += math.ceil(len(bit)/8)
+    return sum_bits
+
+
+
+
 
 def bits_plus_zero(data_num):
     data_bits = []
     for bits in data_num:
         bit = bin(bits)[2:].zfill(len_block)
         data_bits.append(bit)
-        print(len(bit))
+        # print(len(bit))
     return data_bits
 
-def record_cipher(data_bits):
+def record_cipher(data_bits, sum_bits):
     data_bits = "".join(data_bits)
     data_int = int(data_bits, 2)
-    data_bytes = int.to_bytes(data_int, 3584, byteorder='big', signed=True)
+    data_bytes = int.to_bytes(data_int, sum_bits, byteorder='big', signed=True)
+    print(data_bytes[0] == 0)
+    while data_bytes[0] == 0:
+        print(data_bits)
+        data_bytes = data_bytes[1:]
     with open("decrypt_text.txt", "wb") as output_text:
         output_text.write(data_bytes)
 
@@ -61,4 +77,6 @@ len_block = get_len_block(data_privat_key)
 data_blocks_text = splitting_into_bits(len_block_full)
 data_num = decryptor(data_privat_key, data_blocks_text)
 data_bits = bits_plus_zero(data_num)
-record_cipher(data_bits)
+sum_bits = checker_bits(data_num)
+record_cipher(data_bits,sum_bits)
+print(sum_bits)
